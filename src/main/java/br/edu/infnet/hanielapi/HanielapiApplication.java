759829b59a852237ea2014fe9292bc9a51891b00
Haniel Loan <haniel.loan@gmail.com>
2025-10-10 20:56:55 -0300

    first commit

---
+++ src/main/java/br/edu/infnet/hanielapi/HanielapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.hanielapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanielapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanielapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/hanielapi/HanielapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.hanielapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanielapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
